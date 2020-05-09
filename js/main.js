02caf8a2778257b9cadb3041571cc4b8ff6b0814
bsMaria <bmaria.contact@gmail.com>
2020-05-09 14:29:46 -0300

    last version

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//Show and hide the search input
+
 const menuIcon = document.getElementById("menu-icon");
 const slideOutMenu = document.getElementById("slideout-menu");
 const searchIcon = document.getElementById("search-icon");
@@ -5,7 +7,7 @@ const searchBox = document.getElementById("searchbox");
 
 searchBox.style.top = '10px';
 
-searchIcon.addEventListener('click', function() {
+searchIcon.addEventListener('click', function () {
 
   if (searchBox.style.top == '50px') {
     searchBox.style.top = '10px';
@@ -17,12 +19,12 @@ searchIcon.addEventListener('click', function() {
 
 });
 
-menuIcon.addEventListener('click', function() {
-  if(slideOutMenu.style.opacity == "1") {
+menuIcon.addEventListener('click', function () {
+  if (slideOutMenu.style.opacity == "1") {
     slideOutMenu.style.opacity = '0';
     slideOutMenu.style.pointerEvents = 'none';
   } else {
     slideOutMenu.style.opacity = '1';
     slideOutMenu.style.pointerEvents = 'auto';
   }
-})
+});
